95fc49cd0c3ca482848b13ad366aa3d8682da65e
Darioncorah <58603378+Darioncorah@users.noreply.github.com>
2019-12-06 09:44:23 -0700

    Add files via upload

---
+++ Final attempt 1/Final attempt 1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Final_attempt_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gametime = new System.Windows.Forms.Timer(this.components);
+            this.lblpoints = new System.Windows.Forms.Label();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.Atimer = new System.Windows.Forms.Timer(this.components);
+            this.lblhigh = new System.Windows.Forms.Label();
+            this.picrock = new System.Windows.Forms.PictureBox();
+            this.picbrad = new System.Windows.Forms.PictureBox();
+            this.picbrick = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picrock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbrad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbrick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gametime
+            // 
+            this.gametime.Interval = 1;
+            this.gametime.Tick += new System.EventHandler(this.Gametime_Tick);
+            // 
+            // lblpoints
+            // 
+            this.lblpoints.AutoSize = true;
+            this.lblpoints.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpoints.Location = new System.Drawing.Point(12, 9);
+            this.lblpoints.Name = "lblpoints";
+            this.lblpoints.Size = new System.Drawing.Size(116, 27);
+            this.lblpoints.TabIndex = 4;
+            this.lblpoints.Text = "Points: 0";
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(897, 9);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 5;
+            this.btnstart.Text = "Start button";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.Btnstart_Click);
+            // 
+            // Atimer
+            // 
+            this.Atimer.Enabled = true;
+            this.Atimer.Interval = 1;
+            this.Atimer.Tick += new System.EventHandler(this.Atimer_Tick);
+            // 
+            // lblhigh
+            // 
+            this.lblhigh.AutoSize = true;
+            this.lblhigh.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhigh.Location = new System.Drawing.Point(383, 9);
+            this.lblhigh.Name = "lblhigh";
+            this.lblhigh.Size = new System.Drawing.Size(165, 27);
+            this.lblhigh.TabIndex = 7;
+            this.lblhigh.Text = "HighScore: 0";
+            // 
+            // picrock
+            // 
+            this.picrock.Image = global::Final_attempt_1.Properties.Resources.Rock;
+            this.picrock.Location = new System.Drawing.Point(934, 260);
+            this.picrock.Name = "picrock";
+            this.picrock.Size = new System.Drawing.Size(92, 116);
+            this.picrock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picrock.TabIndex = 6;
+            this.picrock.TabStop = false;
+            this.picrock.Visible = false;
+            // 
+            // picbrad
+            // 
+            this.picbrad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.picbrad.Image = global::Final_attempt_1.Properties.Resources.Brad_stick_figure2;
+            this.picbrad.Location = new System.Drawing.Point(70, 368);
+            this.picbrad.Name = "picbrad";
+            this.picbrad.Size = new System.Drawing.Size(47, 145);
+            this.picbrad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbrad.TabIndex = 1;
+            this.picbrad.TabStop = false;
+            // 
+            // picbrick
+            // 
+            this.picbrick.Image = global::Final_attempt_1.Properties.Resources.Brick;
+            this.picbrick.Location = new System.Drawing.Point(934, 446);
+            this.picbrick.Name = "picbrick";
+            this.picbrick.Size = new System.Drawing.Size(38, 74);
+            this.picbrick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbrick.TabIndex = 0;
+            this.picbrick.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Final_attempt_1.Properties.Resources.Backround2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.lblhigh);
+            this.Controls.Add(this.picrock);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.lblpoints);
+            this.Controls.Add(this.picbrad);
+            this.Controls.Add(this.picbrick);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picrock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbrad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbrick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picbrick;
+        private System.Windows.Forms.PictureBox picbrad;
+        private System.Windows.Forms.Timer gametime;
+        private System.Windows.Forms.Label lblpoints;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Timer Atimer;
+        private System.Windows.Forms.PictureBox picrock;
+        private System.Windows.Forms.Label lblhigh;
+    }
+}
+
---
+++ Final attempt 1/Final attempt 1/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_attempt_1
+{
+    public partial class Form1 : Form
+    {
+        bool right;
+        bool left;
+        bool crouch;
+        bool uncrouch;
+        bool jump;
+        int G = 29;
+        int Force;
+        int uod = 0;
+        int pnt = 0;
+        int highscore = 0;
+        Random rnd = new Random();
+
+        void Lose()
+        {
+            gametime.Enabled = false;
+            if (pnt <= 10)
+            {
+                MessageBox.Show("Wow, That was Terrible!!!");
+            }
+            else if (11 <= pnt && pnt <= 20)
+            {
+                MessageBox.Show("Did a Rock hit you in the Face?");
+            }
+            else if (21 <= pnt && pnt <= 30)
+            {
+                MessageBox.Show("Not Bad");
+            }
+            else if (31 <= pnt && pnt <= 40)
+            {
+                MessageBox.Show("Congrats, you're ok");
+            }
+            else
+            {
+                MessageBox.Show("Not sure how you got this far");
+            }
+            picrock.Location = new Point(1000, 260);
+            picbrick.Location = new Point(1000, 446);
+            picbrad.Location = new Point(70, 368);
+            lblpoints.Text = ("Points: " + pnt.ToString());
+            btnstart.Enabled = true;
+            if (pnt > highscore)
+            {
+                highscore = pnt;
+                lblhigh.Text = ("HighScore: " + highscore.ToString());
+            }
+            //resest everything
+            pnt = 0;
+            left = false;
+            right = false;
+            crouch = false;
+            uncrouch = true;
+            picrock.Visible = false;
+        }
+
+        void Brickreset()
+        {
+            //resets the objects
+            if (picbrick.Left <= -300)
+            {
+                picbrick.Location = new Point(1000, 446);
+                picrock.Location = new Point(1000, 260);
+                pnt++;
+                lblpoints.Text = ("Points: " + pnt.ToString());
+                if (pnt >= 11)
+                {
+                    uod = rnd.Next(0, 31);
+                }
+            }
+        }
+
+        void Rockreset()
+        {
+            //resets the objects
+            if (picrock.Left <= -300)
+            {
+                picbrick.Location = new Point(1000, 446);
+                picrock.Location = new Point(1000, 260);
+                pnt++;
+                lblpoints.Text = ("Points: " + pnt.ToString());
+                if (pnt >= 11)
+                {
+                    uod = rnd.Next(0, 31);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Steve Wizardry
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //controls movement
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Down) { crouch = true; }
+            if (e.KeyCode == Keys.Down) { uncrouch = false; }
+            //if jumping, you cant jump
+            if (jump == false)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    jump = true;
+                    Force = G;
+                }
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //disables movement after key release
+            if (e.KeyCode == Keys.Left) { left = false; }
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Down) { crouch = false; }
+            if (e.KeyCode == Keys.Down) { uncrouch = true; }
+        }
+        private void Atimer_Tick(object sender, EventArgs e)
+        {
+            //controls the movement
+            if (crouch == true) { picbrad.Height = 50; }
+            if (uncrouch == true) { picbrad.Height = 150; }
+            if (right == true) { picbrad.Left += 5; }
+            if (left == true) { picbrad.Left -= 5; }
+
+            //force spikes to 29 and rapidly decreses
+            if (jump == true)
+            {
+                picbrad.Top -= Force; //gravity
+                Force -= 1;
+            }
+            if (picbrad.Top + picbrad.Height >= 510)
+            {
+                picbrad.Top = 520 - picbrad.Height;
+                jump = false;
+            }
+            else
+            {
+                picbrad.Top += 5;
+            }
+        }
+
+        private void Gametime_Tick(object sender, EventArgs e)
+        {
+            //if rnd is less than 10 just the brick appears
+            if (uod <= 10)
+            {
+                picbrick.Visible = true;
+                picrock.Visible = false;
+                if (picbrad.Bounds.IntersectsWith(picbrick.Bounds))
+                {
+                    Lose();
+                }
+                Brickreset();
+            }
+            //if rnd is between 11 and 20 just the rock appears
+            if (11 <= uod && uod <= 20)
+            {
+                picbrick.Visible = false;
+                picrock.Visible = true;
+                if (picbrad.Bounds.IntersectsWith(picrock.Bounds))
+                {
+                    Lose();
+                }
+                Brickreset();
+
+            }
+            //if  rnd is between 21 and 25 both appear with the rock moving faster
+            if (21 <= uod && uod <= 25)
+            {
+                picrock.Left -= 2;
+                picbrick.Left += 4;
+                picbrick.Visible = true;
+                picrock.Visible = true;
+                if (picbrad.Bounds.IntersectsWith(picbrick.Bounds))
+                {
+                    Lose();
+                }
+                if (picbrad.Bounds.IntersectsWith(picrock.Bounds))
+                {
+                    Lose();
+                }
+                Brickreset();
+            }
+            //if  rnd is between 26 and 30 both appear with the brick moving faster
+            if (26 <= uod && uod <= 30)
+            {
+                picrock.Left += 4;
+                picbrick.Left -= 4;
+                picbrick.Visible = true;
+                picrock.Visible = true;
+                if (picbrad.Bounds.IntersectsWith(picbrick.Bounds))
+                {
+                    Lose();
+
+                }
+                if (picbrad.Bounds.IntersectsWith(picrock.Bounds))
+                {
+                    Lose();
+                }
+                Rockreset();
+
+            }
+            //controls the speed of the pictureboxes
+            if (pnt <= 10) { picbrick.Left -= 6; picrock.Left -= 6; }
+            else if (11 <= pnt && pnt <= 20) { picbrick.Left -= 8; picrock.Left -= 8; }
+            else if (21 <= pnt && pnt <= 25) { picbrick.Left -= 10; picrock.Left -= 10; }
+            else if (26 <= pnt && pnt <= 30) { picbrick.Left -= 12; picrock.Left -= 12; }
+            else if (31 <= pnt && pnt <= 35) { picbrick.Left -= 14; picrock.Left -= 14; }
+            else if (36 <= pnt && pnt <= 40) { picbrick.Left -= 16; picrock.Left -= 16; }
+            else if (41 <= pnt && pnt <= 45) { picbrick.Left -= 18; picrock.Left -= 18; }
+            else if (46 <= pnt && pnt <= 50) { picbrick.Left -= 20; picrock.Left -= 20; }
+            else if (51 <= pnt && pnt <= 55) { picbrick.Left -= 22; picrock.Left -= 22; }
+            else if (56 <= pnt && pnt <= 60) { picbrick.Left -= 24; picrock.Left -= 24; }
+            else if (61 <= pnt && pnt <= 65) { picbrick.Left -= 26; picrock.Left -= 26; }
+            else if (66 <= pnt && pnt <= 70) { picbrick.Left -= 28; picrock.Left -= 28; }
+        }
+
+        private void Btnstart_Click(object sender, EventArgs e)
+        {
+            gametime.Enabled = true;
+            btnstart.Enabled = false;
+        }
+    }
+}
